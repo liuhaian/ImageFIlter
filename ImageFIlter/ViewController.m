a96658a230db584dc08cc82fffe5ca7b112468ab
liuhaian <liuhaian@gmail.com>
2015-01-04 10:18:25 +0800

    CISepiaTone Filter OK

--- ImageFIlter/ViewController.m
+++ ImageFIlter/ViewController.m
@@ -9,10 +9,28 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *targetIMG;
+@property (weak, nonatomic) IBOutlet UIImageView *resultImg;
 
 @end
 
 @implementation ViewController
+- (IBAction)addFilter:(id)sender {
+    CIContext *context = [CIContext contextWithOptions:nil];               // 1
+    //CIImage *image = [CIImage imageWithContentsOfURL:myURL];               // 2
+    CIImage *image = [[CIImage alloc] initWithImage:[UIImage imageNamed:@"Boston City Flow.jpg"]];
+    CIFilter *filter = [CIFilter filterWithName:@"CISepiaTone"];           // 3
+    [filter setValue:image forKey:kCIInputImageKey];
+    [filter setValue:@0.8f forKey:kCIInputIntensityKey];
+    CIImage *result = [filter valueForKey:kCIOutputImageKey];              // 4
+    CGRect extent = [result extent];
+    CGImageRef cgImage = [context createCGImage:result fromRect:extent];   // 5
+    //UIImage *newIMG=[[UIImage alloc] initWithCIImage:result];
+    UIImage *newIMG=[UIImage imageWithCGImage:[context createCGImage:result fromRect:result.extent]];
+    [self.resultImg setImage:newIMG];
+    //[self.targetIMG setImage:newIMG];
+    //self.resultImg.image=cgImage;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,5 +41,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
